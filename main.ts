37dd2da1fa6529f4001a675c648c86647d613ebf
giano574 <17013720+giano574@users.noreply.github.com>
2021-03-22 21:07:08 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,30 @@
+input.onButtonPressed(Button.A, function () {
+    change_target(-10)
+})
+function change_target (value: number) {
+    target_light = Math.min(255, Math.max(0, target_light + value))
+    show_target()
+}
+radio.onReceivedString(function (receivedString) {
+    serial.writeLine(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    change_target(10)
+})
+function show_target () {
+    led.plotBarGraph(
+    target_light,
+    255
+    )
+}
+let target_light = 0
+radio.setGroup(1)
+let id = "m"
+target_light = Math.round(255 / 2)
+show_target()
 basic.forever(function () {
-	
+    if (Math.abs(input.lightLevel() - target_light) > 0) {
+        serial.writeLine("" + id + "," + input.lightLevel() + "," + target_light)
+        basic.pause(10000)
+    }
 })
